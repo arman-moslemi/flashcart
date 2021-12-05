cab628ee06fe2db215b608701b322fb880cdec03
amir333 <a.mofid76@yahoo.com>
2021-12-05 08:18:07 -0800

    ss

--- ios/FlashCard/AppDelegate.m
+++ ios/FlashCard/AppDelegate.m
@@ -47,6 +47,15 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  for (NSString* family in [UIFont familyNames])
+{
+    NSLog(@"%@", family);
+
+    for (NSString* name in [UIFont fontNamesForFamilyName: family])
+    {
+        NSLog(@"Family name:  %@", name);
+    }
+}
   return YES;
 }
 
@@ -58,5 +67,6 @@ static void InitializeFlipper(UIApplication *application) {
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
+
 
 @end
